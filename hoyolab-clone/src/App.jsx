e8a08bc766c692f3fe20abf000ac3b96ef90be2d
JigmeZ <jigmechogyal2006@gmail.com>
2025-04-20 18:38:59 +0530

    feat(sidebar): updated app.jsx and app.css

--- hoyolab-clone/src/App.jsx
+++ hoyolab-clone/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Sidebar from './Components/Sidebar'
 
 function App() {
   return (
     <>
       <Navbar />
-      
+      <div className="app-layout"> {/* Updated layout */}
+        <Sidebar /> {/* Sidebar moved to the left */}
+        <main className="main-content"> {/* Main content comes after Sidebar */}
+          <h1>Welcome to Hoyolab Clone</h1>
+          <p>Explore the features of Hoyolab.</p>
+        </main>
+      </div>
     </>
   )
 }
