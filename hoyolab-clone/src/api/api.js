e64775e777b8e57cad507c700fdccce14b78c12d
PemaLoselMaurer <fritzleemaurer@gmail.com>
2025-06-08 21:40:22 +0600

    random

--- hoyolab-clone/src/api/api.js
+++ hoyolab-clone/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000/api"; // Changed to match backend
+const API_BASE_URL = "http://localhost:4000/api"; // Make sure this matches your backend port
 
 export const fetchPosts = async () => {
   const response = await axios.get(`${API_BASE_URL}/posts`);
