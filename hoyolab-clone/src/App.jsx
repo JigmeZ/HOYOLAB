53463ac59f425957419052f9907c9e35e9f0dcea
Sherab nima Rigzin <02240358.cst@rub.gov.bt>
2025-06-10 04:23:17 -0700

    last commit

--- hoyolab-clone/src/App.jsx
+++ hoyolab-clone/src/App.jsx
@@ -30,6 +30,8 @@ function App() {
             <>
               <Tabs />
               <div className="app-layout">
+                <Sidebar />
+
               </div>
             </>
           }
